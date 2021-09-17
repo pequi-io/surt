296f02d2d813c4a0263bb37b1d490f205791ebc6
leandrocostam <leandrocostam@gmail.com>
2021-09-17 21:18:02 +0200

    feat: Add task controller

--- pkg/types/object.go
+++ pkg/types/object.go
@@ -2,17 +2,17 @@ package types
 
 import (
 	"strings"
-	"time"
 )
 
 //Object type
 type Object struct {
-	RawPath    string //Raw Storage Object Key value. Example: s3://mybucket/path-to-file/file.exe
-	SOK        SOK
-	Content    []byte
-	Tags       map[string]string
-	ScanResult string
-	LastScanAt time.Time
+	RawFilePath string //Raw Storage Object Key value. Example: s3://mybucket/path-to-file/file.exe
+	SOK         SOK
+	Content     []byte
+	ContentHash string
+	SizeMb      float32
+	Tags        map[string]string
+	Scan        Scan
 }
 
 //Storage Object Key (SOK)
@@ -22,11 +22,11 @@ type SOK struct {
 	Key            string
 }
 
-//GenerateSOK converts RawPath string to SOK type
+//GenerateSOK converts RawFilePath string to SOK type
 func (o *Object) GenerateSOK() {
 	var s SOK
-	s.StorageService = strings.Split(o.RawPath, ":")[0]
-	tmpStr := string(strings.Split(o.RawPath, "//")[1])
+	s.StorageService = strings.Split(o.RawFilePath, ":")[0]
+	tmpStr := string(strings.Split(o.RawFilePath, "//")[1])
 	s.BucketName = string(strings.SplitN(tmpStr, "/", 2)[0])
 	s.Key = string(strings.SplitN(tmpStr, "/", 2)[1])
 	o.SOK = s
