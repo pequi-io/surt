d9de82e850bff4aa99f5b0f05651c5ae7ad81509
brunopadz <bpadz@protonmail.com>
2021-10-12 11:17:40 -0300

    fix: updates internal package references
    Signed-off-by: brunopadz <bpadz@protonmail.com>

--- pkg/storage/services/s3/s3.go
+++ pkg/storage/services/s3/s3.go
@@ -8,8 +8,8 @@ import (
 	awshelper "github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
-	"github.com/surt-io/surt/pkg/providers/aws"
-	"github.com/surt-io/surt/pkg/util/logger"
+	"github.com/surt-io/surt/pkg/cloudproviders/aws"
+	"github.com/surt-io/surt/pkg/logger"
 )
 
 type S3GetObject interface {
